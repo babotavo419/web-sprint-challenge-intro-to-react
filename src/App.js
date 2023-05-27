333f7c9565ca01cd930a0dfcc343a16d001c3348
babotavo419 <one.faith419@gmail.com>
2023-05-27 07:10:26 -0400

    needs work

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import axios from 'axios';
+import Character from './components/Character';
+import { useState, useEffect } from 'react';
+
+const App = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      try {
+        const response = await axios.get('https://swapi.dev/api/people/');
+        setCharacters(response.data.results);
+
+        console.log(response.data.results);
+      } catch (error) {
+        console.error('Error fetching characters:', error);
+      }
+    };
+
+    fetchCharacters();
+  }, []);
+
+  return (
+    <div>
+      {characters.map((character) => (
+  <Character key={character.id} character={character} />
+))}
+
+    </div>
+  );
+};
+
+export default App;
+
+
+
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,45 @@
 // Write your Character component here
+// Character.js
+
+import React, { useState } from 'react';
+
+const Character = ({ character }) => {
+    const [expanded, setExpanded] = useState(false);
+  
+    const toggleExpanded = () => {
+      setExpanded(!expanded);
+    };
+  
+    return (
+      <div className="character-container">
+        <button
+          onClick={toggleExpanded}
+          style={{
+            padding: '0.5rem',
+            backgroundColor: 'transparent',
+            alignContent: 'center',
+            justifyContent: 'center',
+            border: '5px solid black',
+            margin: '0.5rem',
+          }}
+          className={`button ${expanded ? 'button-collapsed' : ''}`}
+        >
+          {character.name}
+        </button>
+        {expanded && (
+          <div className="character-details">
+            <p>Height: {character.height}</p>
+            <p>Mass: {character.mass}</p>
+            <p>Hair Color: {character.hair_color}</p>
+            <p>Skin Color: {character.skin_color}</p>
+            <p>Eye Color: {character.eye_color}</p>
+            <p>Birth Year: {character.birth_year}</p>
+            <p>Gender: {character.gender}</p>
+          </div>
+        )}
+      </div>
+    );
+  };
+  
+  export default Character;
+  
