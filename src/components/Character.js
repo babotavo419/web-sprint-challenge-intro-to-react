5098f8c77a083f2bc15dceaef14d4d4329464188
babotavo419 <one.faith419@gmail.com>
2023-05-27 07:49:45 -0400

    it broke agian now its working againg

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,14 +1,31 @@
 // Write your Character component here
 // Character.js
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Character = ({ character }) => {
   const [expanded, setExpanded] = useState(false);
+  const [characterInfo, setCharacterInfo] = useState(null);
 
   const toggleExpanded = () => {
     setExpanded(!expanded);
   };
+
+  useEffect(() => {
+    const fetchCharacterInfo = async () => {
+      try {
+        const response = await fetch(character.url);
+        const data = await response.json();
+        setCharacterInfo(data);
+      } catch (error) {
+        console.error('Error fetching character information:', error);
+      }
+    };
+
+    if (expanded && !characterInfo) {
+      fetchCharacterInfo();
+    }
+  }, [expanded, character.url, characterInfo]);
 
   return (
     <div className="character-container">
@@ -24,17 +41,17 @@ const Character = ({ character }) => {
         }}
         className={`button ${expanded ? 'button-collapsed' : ''}`}
       >
-        <span>{character.name}</span>
+        {character.name}
       </button>
-      {expanded && (
+      {expanded && characterInfo && (
         <div className="character-details">
-          <p>Height: {character.height}</p>
-          <p>Mass: {character.mass}</p>
-          <p>Hair Color: {character.hair_color}</p>
-          <p>Skin Color: {character.skin_color}</p>
-          <p>Eye Color: {character.eye_color}</p>
-          <p>Birth Year: {character.birth_year}</p>
-          <p>Gender: {character.gender}</p>
+          <p>Height: {characterInfo.height}</p>
+          <p>Mass: {characterInfo.mass}</p>
+          <p>Hair Color: {characterInfo.hair_color}</p>
+          <p>Skin Color: {characterInfo.skin_color}</p>
+          <p>Eye Color: {characterInfo.eye_color}</p>
+          <p>Birth Year: {characterInfo.birth_year}</p>
+          <p>Gender: {characterInfo.gender}</p>
         </div>
       )}
     </div>
@@ -42,6 +59,7 @@ const Character = ({ character }) => {
 };
 
 export default Character;
+
 
 
   
