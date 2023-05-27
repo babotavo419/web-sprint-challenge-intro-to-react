4e600cb59a94a77de65cbb7eb35b987122e389de
babotavo419 <one.faith419@gmail.com>
2023-05-27 07:34:02 -0400

    hope this helps

--- src/components/Character.js
+++ src/components/Character.js
@@ -10,44 +10,38 @@ const Character = ({ character }) => {
     setExpanded(!expanded);
   };
 
-  const buttonStyles = {
-    padding: '0.5rem',
-    backgroundColor: 'transparent',
-    alignContent: 'center',
-    justifyContent: 'center',
-    border: '5px solid black',
-    margin: '0.5rem',
-  };
-
-  const characterDetailsStyles = {
-    display: expanded ? 'block' : 'none',
-    margin: '1rem 0',
-    backgroundColor: '#f2f2f2',
-    padding: '1rem',
-  };
-
   return (
     <div className="character-container">
       <button
         onClick={toggleExpanded}
-        style={buttonStyles}
+        style={{
+          padding: '0.5rem',
+          backgroundColor: 'transparent',
+          alignContent: 'center',
+          justifyContent: 'center',
+          border: '5px solid black',
+          margin: '0.5rem',
+        }}
         className={`button ${expanded ? 'button-collapsed' : ''}`}
       >
-        {character.name}
+        <span>{character.name}</span>
       </button>
-      <div style={characterDetailsStyles} className="character-details">
-        <p>Height: {character.height}</p>
-        <p>Mass: {character.mass}</p>
-        <p>Hair Color: {character.hair_color}</p>
-        <p>Skin Color: {character.skin_color}</p>
-        <p>Eye Color: {character.eye_color}</p>
-        <p>Birth Year: {character.birth_year}</p>
-        <p>Gender: {character.gender}</p>
-      </div>
+      {expanded && (
+        <div className="character-details">
+          <p>Height: {character.height}</p>
+          <p>Mass: {character.mass}</p>
+          <p>Hair Color: {character.hair_color}</p>
+          <p>Skin Color: {character.skin_color}</p>
+          <p>Eye Color: {character.eye_color}</p>
+          <p>Birth Year: {character.birth_year}</p>
+          <p>Gender: {character.gender}</p>
+        </div>
+      )}
     </div>
   );
 };
 
 export default Character;
+
 
   
