d0290cfade01dceb29ce87cf628fc85f13670f47
babotavo419 <one.faith419@gmail.com>
2023-05-29 11:52:12 -0400

    cooking with bacon grease

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import styles from './Characters.module.css';
 
 const Characters = ({ charactersData }) => {
   return (
     <div>
       {charactersData.map((character, index) => (
-        <div key={index}>
-          <h2>{character.name}</h2>
-          <p>{character.height}</p>
-          <p>{character.mass}</p>
-          <p>{character.hair_color}</p>
-          <p>{character.skin_color}</p>
+        <div key={index} className={styles.container}>
+          <h2 className={styles.characterName}>{character.name}</h2>
+          <p className={styles.characterInfo}>Height: {character.height}</p>
+          <p className={styles.characterInfo}>Mass: {character.mass}</p>
+          <p className={styles.characterInfo}>Hair color: {character.hair_color}</p>
+          <p className={styles.characterInfo}>Skin color: {character.skin_color}</p>
         </div>
       ))}
     </div>
@@ -17,3 +18,4 @@ const Characters = ({ charactersData }) => {
 };
 
 export default Characters;
+
